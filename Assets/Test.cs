ca9bbf5378c7769c236636a227305ab4cafffda9
Chuan Gao <chuangao@Chuans-MacBook-Pro.local>
2016-07-23 20:19:05 +0800

    add demo project

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Test : MonoBehaviour {
+
+	public GameObject targetText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnSave()
+	{
+		PlayerPrefs.SetString ("testme", "testme");
+		PlayerPrefs.Save ();
+	}
+
+	public void OnLoad()
+	{
+		Text target = targetText.GetComponent<Text> ();
+		target.text = PlayerPrefs.GetString ("testme");
+	}
+
+	public void OnClean()
+	{
+		Caching.CleanCache ();
+	}
+}
